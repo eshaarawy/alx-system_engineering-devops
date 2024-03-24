9b72d20b8a76a13d3ff8e4b8a37ad57980555718
Mohamed Elshaarawy <elsharawy234@gmail.com>
2024-03-24 22:29:05 +0200

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-#!/usr/bin/pup
-# install a package
-package { 'flask':
-            ensure   => '2.1.0',
-            provider => 'pip3'
+# Install puppet-lint
+
+package { 'puppet-lint':
+    ensure   => '2.1.1',
+    provider => 'gem',
 }
